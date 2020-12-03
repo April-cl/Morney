7326821037bcd429ca7fc26267c9096a667c0ed5
April-cl <xcl8202@gmail.com>
2020-12-04 00:54:55 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    Money
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+    </div>
     <Nav />
   </div>
 </template>
@@ -12,4 +27,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  .content {
+    border: 1px solid blue;
+    overflow: auto;
+    flex-grow: 1;
+  }
+}
 </style>
