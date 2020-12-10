8e44e816895e617a640a64ae7e25ee37717c30f6
April-cl <xcl8202@gmail.com>
2020-12-11 01:32:12 +0800

    把record的复制保存到localStorage并解决NumberPad中返回value值警告

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -63,7 +63,7 @@ export default class NumberPad extends Vue {
   }
 
   ok() {
-    this.$emit("update:value", this.output.toString());
+    this.$emit("update:value", parseFloat(this.output)).$emit("submit");
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,11 +1,11 @@
 import content from '../shims-vue';
 <template>
   <Layout class-prefix="layout">
-    <NumberPad :value.sync="record.amount" />
+    <NumberPad :value.sync="record.amount" @submit="saveRecord" />
     <Types :value.sync="record.type" />
     <Notes @update:value="onUpdateNotes" />
     <Tags @update:value="onUpdateTags" :data-source.sync="tags" />
-    {{record}}
+    {{recordList}}
   </Layout>
 </template>
 
@@ -15,7 +15,7 @@ import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
-import { Component } from "vue-property-decorator";
+import { Component, Watch } from "vue-property-decorator";
 
 type Record = {
   tags: string[];
@@ -29,6 +29,8 @@ type Record = {
 })
 export default class Money extends Vue {
   tags: string[] = ["衣", "食", "住", "行"];
+
+  recordList: Record[] = [];
 
   record: Record = {
     tags: [],
@@ -45,12 +47,14 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
 
-  onUpdateTypes(value: string) {
-    this.record.type = value;
+  saveRecord() {
+    // this.recordList.push(this.record); 这种写法由bug
+    this.recordList.push(JSON.parse(JSON.stringify(this.record)));
   }
 
-  onUpdateAmount(value: number) {
-    this.record.amount = value;
+  @Watch("recordList")
+  onRecordListChange() {
+    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
   }
 }
 </script>
