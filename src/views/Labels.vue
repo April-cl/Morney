885df9d064f51e8d78ae16c03be490292b143223
April-cl <xcl8202@gmail.com>
2020-12-15 22:12:23 +0800

    完成Label页面的新建标签功能

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -8,7 +8,7 @@ import Icon from '@/components/Icon.vue';
       </li>
     </ol>
     <div class="createTag-wrapper">
-      <button class="createTag">新建标签</button>
+      <button class="createTag" @click="createTag">新建标签</button>
     </div>
   </Layout>
 </template>
@@ -16,10 +16,24 @@ import Icon from '@/components/Icon.vue';
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import tagListModel from "../models/tagListModel";
 
+tagListModel.fetch();
 @Component
 export default class Labels extends Vue {
-  tags: string[] = ["衣", "食", "住", "行"];
+  tags = tagListModel.data;
+
+  createTag() {
+    const name = window.prompt("请输入标签名");
+    if (name) {
+      const message = tagListModel.create(name);
+      if (message === "duplicated") {
+        window.alert("这个标签已经有啦");
+      } else if (message === "success") {
+        window.alert("添加成功");
+      }
+    }
+  }
 }
 </script>
 
