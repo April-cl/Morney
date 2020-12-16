71b4e661e2b52ae28ce0b329fe5ef5855ff9e2eb
April-cl <xcl8202@gmail.com>
2020-12-17 02:30:21 +0800

    value从data变成了prop

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,11 +1,11 @@
-import content from '../shims-vue';
 <template>
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
     <Types :value.sync="record.type" />
-    <Notes field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes" />
+    <div class="notes">
+      <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes" />
+    </div>
     <Tags @update:value="onUpdateTags" :data-source.sync="tags" />
-    {{recordList}}
   </Layout>
 </template>
 
@@ -13,7 +13,7 @@ import content from '../shims-vue';
 import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
-import Notes from "@/components/Money/Notes.vue";
+import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component, Watch } from "vue-property-decorator";
 import recordListModel from "@/models/recordListModel.ts";
@@ -22,7 +22,7 @@ import tagListModel from "../models/tagListModel";
 const recordList = recordListModel.fetch();
 
 @Component({
-  components: { Tags, Notes, Types, NumberPad },
+  components: { Tags, FormItem, Types, NumberPad },
 })
 export default class Money extends Vue {
   tags = tagListModel.data;
@@ -60,5 +60,8 @@ export default class Money extends Vue {
 .layout-content {
   display: flex;
   flex-direction: column-reverse;
+}
+.notes {
+  padding: 12px 0;
 }
 </style>
